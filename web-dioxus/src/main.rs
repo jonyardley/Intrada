30124832b765b120e4444971003247b1787a3b02
Jon Yardley <jonyardley@Jons-MacBook-Pro.local>
2025-02-08 07:52:10 +0000

    Base styles applied

--- web-dioxus/src/main.rs
+++ web-dioxus/src/main.rs
@@ -12,7 +12,10 @@ enum Route {
 }
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
-const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
+const DAISY_CSS: &str = "https://cdn.jsdelivr.net/npm/daisyui@5.0.0-beta.7/daisyui.css";
+const DAISY_THEMES: &str = "https://cdn.jsdelivr.net/npm/daisyui@5.0.0-beta.7/themes.css";
+const TAILWIND_CSS: &str = "https://cdn.tailwindcss.com";
+const APP_CSS: Asset = asset!("/assets/styling/main.css");
 
 fn main() {
     dioxus::launch(App);
@@ -22,8 +25,10 @@ fn main() {
 fn App() -> Element {
     rsx! {
         document::Link { rel: "icon", href: FAVICON }
+        document::Link { rel: "stylesheet", href: DAISY_CSS }
+        document::Link { rel: "stylesheet", href: DAISY_THEMES }
         document::Link { rel: "stylesheet", href: TAILWIND_CSS }
-
-        Router::<Route> {}
+        document::Link { rel: "stylesheet", href: APP_CSS }
+        div { "data-theme": "dark", Router::<Route> {} }
     }
 }
--- web-dioxus/src/views/home.rs
+++ web-dioxus/src/views/home.rs
@@ -3,7 +3,78 @@ use dioxus::prelude::*;
 
 #[component]
 pub fn Home() -> Element {
-    rsx! {
-        h1 { "Welcome to Dioxus!" }
-    }
+    rsx!(
+        div { class: "navbar bg-base-100",
+            div { class: "navbar-start",
+                div { class: "dropdown",
+                    div {
+                        tabindex: "0",
+                        role: "button",
+                        class: "btn btn-ghost lg:hidden",
+                        svg {
+                            xmlns: "http://www.w3.org/2000/svg",
+                            class: "h-5 w-5",
+                            fill: "none",
+                            view_box: "0 0 24 24",
+                            stroke: "currentColor",
+                            path {
+                                stroke_linecap: "round",
+                                stroke_linejoin: "round",
+                                stroke_width: "2",
+                                d: "M4 6h16M4 12h8m-8 6h16",
+                            }
+                        }
+                    }
+                    ul {
+                        tabindex: "0",
+                        class: "menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow",
+                        li {
+                            a { "Item 1" }
+                        }
+                        li {
+                            a { "Parent" }
+                            ul { class: "p-2",
+                                li {
+                                    a { "Submenu 1" }
+                                }
+                                li {
+                                    a { "Submenu 2" }
+                                }
+                            }
+                        }
+                        li {
+                            a { "Item 3" }
+                        }
+                    }
+                }
+                a { class: "btn btn-ghost text-xl", "Practice App" }
+            }
+            div { class: "navbar-center hidden lg:flex",
+                ul { class: "menu menu-horizontal px-1",
+                    li {
+                        a { "Item 1" }
+                    }
+                    li {
+                        details {
+                            summary { "Parent" }
+                            ul { class: "p-2",
+                                li {
+                                    a { "Submenu 1" }
+                                }
+                                li {
+                                    a { "Submenu 2" }
+                                }
+                            }
+                        }
+                    }
+                    li {
+                        a { "Item 3" }
+                    }
+                }
+            }
+            div { class: "navbar-end",
+                a { class: "btn", "Login" }
+            }
+        }
+    )
 }
