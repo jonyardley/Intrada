45e872ea5c7d4414d758f62a0bbaa0a88d634374
Jon Yardley <jonyardley@Jons-MacBook-Pro.local>
2025-02-15 09:12:42 +0000

    Only need swift for now

--- shared_types/build.rs
+++ shared_types/build.rs
@@ -13,9 +13,9 @@ fn main() -> anyhow::Result<()> {
 
     gen.swift("SharedTypes", output_root.join("swift"))?;
 
-    gen.java("com.example.counter.shared_types", output_root.join("java"))?;
+    // gen.java("com.example.counter.shared_types", output_root.join("java"))?;
 
-    gen.typescript("shared_types", output_root.join("typescript"))?;
+    // gen.typescript("shared_types", output_root.join("typescript"))?;
 
     Ok(())
 }
