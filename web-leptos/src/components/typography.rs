48e4d16b2f8cb978460b8b6836f8fc7221ad70b0
Jon Yardley <jonyardley@me.com>
2025-03-12 12:30:45 +0000

    Add form and state to form ready to crux core

--- web-leptos/src/components/typography.rs
+++ web-leptos/src/components/typography.rs
@@ -2,5 +2,5 @@ use leptos::prelude::*;
 
 #[component]
 pub fn Header1(text: &'static str) -> impl IntoView {
-    view! { <h1 class="text-2xl font-bold mb-2">{text.to_string()}</h1> }
+    view! { <h1 class="text-2xl font-bold mb-5">{text.to_string()}</h1> }
 }
--- web-leptos/src/views/exercises.rs
+++ web-leptos/src/views/exercises.rs
@@ -3,8 +3,22 @@ use leptos::prelude::*;
 
 #[component]
 pub fn Exercises() -> impl IntoView {
+    let (exercise_name, set_exercise_name) = signal("".to_string());
+
     view! {
         <Header1 text="Exercises" />
+        <section class="mb-5">
+            <input
+                type="text"
+                class="input"
+                placeholder="Exercise Name"
+                on:input:target=move |ev| {
+                    set_exercise_name.set(ev.target().value());
+                }
+            />
+            <button class="btn">Add Exercise</button>
+        </section>
+        <p>{exercise_name}</p>
         <section>
             <ul>
                 <li>Exercise 1</li>
