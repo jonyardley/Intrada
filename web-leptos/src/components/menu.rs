fafa4bf30cb3feee728637bbceee0991a73e6d70
Jon Yardley <jonyardley@me.com>
2025-03-30 08:45:57 +0100

    Add unique values to menu

--- web-leptos/src/components/menu.rs
+++ web-leptos/src/components/menu.rs
@@ -26,9 +26,13 @@ pub fn Nav() -> impl IntoView {
                         "Goals"
                     </NavCategoryItem>
 <<<<<<< HEAD
+<<<<<<< HEAD
                     <NavSubItem value="manage-goals" href="/goals">"Manage Goals"</NavSubItem>
 =======
                     <NavSubItem value="goals" href="/goals">
+=======
+                    <NavSubItem value="manage-goals" href="/goals">
+>>>>>>> fc679eb (Add unique values to menu)
                         "Manage Goals"
                     </NavSubItem>
 >>>>>>> dabcf06 (Updated refs in menu)
@@ -38,13 +42,17 @@ pub fn Nav() -> impl IntoView {
                         "Sessions"
                     </NavCategoryItem>
 <<<<<<< HEAD
+<<<<<<< HEAD
                     <NavSubItem value="start-session" href="/sessions/new">"Start a session"</NavSubItem>
                     <NavSubItem value="manage-sessions" href="/sessions">"Manage sessions"</NavSubItem>
 =======
                     <NavSubItem value="sessions" href="/sessions/new">
+=======
+                    <NavSubItem value="start-session" href="/sessions/new">
+>>>>>>> fc679eb (Add unique values to menu)
                         "Start a session"
                     </NavSubItem>
-                    <NavSubItem value="sessions" href="/sessions">
+                    <NavSubItem value="manage-sessions" href="/sessions">
                         "Manage sessions"
                     </NavSubItem>
 >>>>>>> dabcf06 (Updated refs in menu)
@@ -54,9 +62,13 @@ pub fn Nav() -> impl IntoView {
                         "Exercises"
                     </NavCategoryItem>
 <<<<<<< HEAD
+<<<<<<< HEAD
                     <NavSubItem value="manage-exercises" href="/exercises">"Manage exercises"</NavSubItem>
 =======
                     <NavSubItem value="exercises" href="/exercises">
+=======
+                    <NavSubItem value="manage-exercises" href="/exercises">
+>>>>>>> fc679eb (Add unique values to menu)
                         "Manage exercises"
                     </NavSubItem>
 >>>>>>> dabcf06 (Updated refs in menu)
