365670dd27be04e75e2755c0366b46ba76091c49
Jon Yardley <jonyardley@me.com>
2025-03-30 08:44:39 +0100

    Updated refs in menu

--- web-leptos/src/components/menu.rs
+++ web-leptos/src/components/menu.rs
@@ -3,7 +3,12 @@ use leptos::prelude::*;
 use thaw::*;
 
 #[component]
+<<<<<<< HEAD
 pub fn nav() -> impl IntoView {
+=======
+pub fn Nav() -> impl IntoView {
+    //Start with all categories open
+>>>>>>> dabcf06 (Updated refs in menu)
     let open_categories = RwSignal::new(vec![
         "goals".to_string(),
         "sessions".to_string(),
@@ -20,20 +25,41 @@ pub fn nav() -> impl IntoView {
                     <NavCategoryItem slot icon=icondata::AiAreaChartOutlined>
                         "Goals"
                     </NavCategoryItem>
+<<<<<<< HEAD
                     <NavSubItem value="manage-goals" href="/goals">"Manage Goals"</NavSubItem>
+=======
+                    <NavSubItem value="goals" href="/goals">
+                        "Manage Goals"
+                    </NavSubItem>
+>>>>>>> dabcf06 (Updated refs in menu)
                 </NavCategory>
                 <NavCategory value="sessions">
                     <NavCategoryItem slot icon=icondata::AiPieChartOutlined>
                         "Sessions"
                     </NavCategoryItem>
+<<<<<<< HEAD
                     <NavSubItem value="start-session" href="/sessions/new">"Start a session"</NavSubItem>
                     <NavSubItem value="manage-sessions" href="/sessions">"Manage sessions"</NavSubItem>
+=======
+                    <NavSubItem value="sessions" href="/sessions/new">
+                        "Start a session"
+                    </NavSubItem>
+                    <NavSubItem value="sessions" href="/sessions">
+                        "Manage sessions"
+                    </NavSubItem>
+>>>>>>> dabcf06 (Updated refs in menu)
                 </NavCategory>
                 <NavCategory value="exercises">
                     <NavCategoryItem slot icon=icondata::AiAreaChartOutlined>
                         "Exercises"
                     </NavCategoryItem>
+<<<<<<< HEAD
                     <NavSubItem value="manage-exercises" href="/exercises">"Manage exercises"</NavSubItem>
+=======
+                    <NavSubItem value="exercises" href="/exercises">
+                        "Manage exercises"
+                    </NavSubItem>
+>>>>>>> dabcf06 (Updated refs in menu)
                 </NavCategory>
             </NavDrawer>
         </Flex>
