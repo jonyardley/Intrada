3a8ad5fa6d76879a4e81bf097bc694ea9ff2c4b3
Jon Yardley <jonyardley@me.com>
2025-04-05 08:47:13 +0100

    active goals placeholder

--- web-leptos/src/components/mod.rs
+++ web-leptos/src/components/mod.rs
@@ -6,3 +6,6 @@ pub use nav::Nav;
 
 pub mod typography;
 pub use typography::{CardTitle, Header1, Header2};
+
+pub mod goal_card;
+pub use goal_card::GoalCard;
--- web-leptos/src/views/home.rs
+++ web-leptos/src/views/home.rs
@@ -1,13 +1,31 @@
 use leptos::prelude::*;
 
+use crate::components::GoalCard;
 use crate::components::Header;
 
 #[component]
 pub fn Home() -> impl IntoView {
     view! {
         <Header />
-        <main>
-            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">"This is the home page"</div>
+        <main class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <h2 class="text-2xl font-bold">"Here are your active goals"</h2>
+            <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+                <GoalCard
+                    title="Goal 1".to_string()
+                    description="Description 1".to_string()
+                    progress=50
+                />
+                <GoalCard
+                    title="Goal 2".to_string()
+                    description="Description 2".to_string()
+                    progress=75
+                />
+                <GoalCard
+                    title="Goal 3".to_string()
+                    description="Description 3".to_string()
+                    progress=100
+                />
+            </div>
         </main>
     }
 }
