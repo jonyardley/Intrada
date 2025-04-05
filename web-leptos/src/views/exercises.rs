6190ce9511d1430dc783167e3317fdb63100897d
Jon Yardley <jonyardley@me.com>
2025-04-05 09:03:06 +0100

    Refactor other modules to use use_core

--- web-leptos/src/views/exercises.rs
+++ web-leptos/src/views/exercises.rs
@@ -1,30 +1,19 @@
-use crate::components::typography::Header1;
 use leptos::prelude::*;
 
-use crate::core;
-use crate::GlobalState;
-use reactive_stores::Store;
+use crate::components::typography::Header1;
+use crate::hooks::use_core;
+use shared::Event;
 
 #[component]
 pub fn Exercises() -> impl IntoView {
-    let state = expect_context::<Store<GlobalState>>().get_untracked();
-    let core = state.core;
-    let (view, render) = signal(core.view());
-    let (event, set_event) = signal(shared::Event::GetExercises);
-
+    let (view, set_event) = use_core(Event::GetExercises);
     let (exercise_name, set_exercise_name) = signal("".to_string());
-
-    Effect::new(move |_| {
-        core::update(&core, event.get(), render);
-    });
 
     view! {
         <div class="drawer">
             <input id="my-drawer" type="checkbox" class="drawer-toggle" />
             <div class="drawer-content">
-
                 <Header1 text="Exercises".to_string() />
-
                 <label for="my-drawer" class="btn btn-primary btn-sm mb-4">
                     "Add new exercise"
                 </label>
@@ -47,44 +36,38 @@ pub fn Exercises() -> impl IntoView {
             </div>
             <div class="drawer-side">
                 <label for="my-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
-
                 <div class="menu bg-base-200 text-base-content min-h-full p-4">
                     <fieldset class="fieldset w-xs bg-base-200 border border-base-300 p-4 mb-4">
                         <legend class="fieldset-legend">"Add new exercise"</legend>
-
                         <input
                             type="text"
                             class="input"
-                            placeholder="Exercise Name"
+                            placeholder="New exercise name"
                             bind:value=(exercise_name, set_exercise_name)
                         />
-
                         <div class="join">
-                            <label
-                                for="my-drawer"
-                                class="btn btn-primary drawer-button mr-2 btn-sm"
+                            <button
+                                class="btn btn-primary btn-sm"
                                 on:click=move |_| {
                                     set_event
                                         .update(|value| {
-                                            *value = shared::Event::AddExercise(exercise_name.get());
+                                            *value = Event::AddExercise(exercise_name.get());
+                                            set_exercise_name.set("".to_string());
                                         });
-                                    set_exercise_name.set("".to_string());
                                 }
                             >
                                 "Add exercise"
-                            </label>
-                            <label
-                                for="my-drawer"
-                                class="btn btn-error btn-accent btn-outline drawer-button btn-sm"
+                            </button>
+                            <button
+                                class="btn btn-error btn-accent btn-outline drawer-button btn-sm ml-2"
                                 on:click=move |_| {
                                     set_exercise_name.set("".to_string());
                                 }
                             >
                                 "Cancel"
-                            </label>
+                            </button>
                         </div>
                     </fieldset>
-
                 </div>
             </div>
         </div>
