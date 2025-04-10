5e4e2616a11e450815ce1366a4817f8e675bb7d2
Jon Yardley <jonyardley@me.com>
2025-04-10 18:10:57 +0100

    Working add goal form

--- web-leptos/src/views/home.rs
+++ web-leptos/src/views/home.rs
@@ -15,26 +15,25 @@ pub fn Home() -> impl IntoView {
                 <div class="mx-auto w-screen max-w-screen-xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8 lg:py-32">
                     <div class="mx-auto max-w-prose text-center">
                         <h1 class="text-4xl font-bold text-gray-900 sm:text-5xl">
-                            "What do you want to "<strong class="text-indigo-600">"achieve"</strong>
-                            " today?"
+                            "Can you "<strong class="text-indigo-600">"Rachmaninov?"</strong>
                         </h1>
 
                         <p class="mt-4 text-base text-pretty text-gray-700 sm:text-lg/relaxed">
-                            "Create a new pactice goal, or dive into an existing one."
+                            "Let's get you there..."
                         </p>
 
                         <div class="mt-4 flex justify-center gap-4 sm:mt-6">
                             <A
                                 attr:class="inline-block rounded border border-indigo-600 bg-indigo-600 px-5 py-3 font-medium text-white shadow-sm transition-colors hover:bg-indigo-700"
-                                href="/goals/new"
+                                href="/sessions"
                             >
-                                "Create a new goal"
+                                "Start a session"
                             </A>
                             <A
                                 attr:class="inline-block rounded border border-gray-200 px-5 py-3 font-medium text-gray-700 shadow-sm transition-colors hover:bg-gray-50 hover:text-gray-900"
-                                href="/goals"
+                                href="/goals/new"
                             >
-                                "View existing goals"
+                                "Create a new goal"
                             </A>
                         </div>
                     </div>
