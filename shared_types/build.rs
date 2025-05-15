a5c1c8d325f8062889fe7324bf53fd95dce024b5
Jon Yardley <jonyardley@me.com>
2025-05-15 15:20:07 +0100

    Update crate and re-add shared types for iios

--- shared_types/build.rs
+++ shared_types/build.rs
@@ -1,6 +1,6 @@
 use crux_core::typegen::TypeGen;
 use shared::Chopin;
-// use std::path::PathBuf;
+use std::path::PathBuf;
 
 fn main() -> anyhow::Result<()> {
     println!("cargo:rerun-if-changed=../shared");
@@ -9,9 +9,9 @@ fn main() -> anyhow::Result<()> {
 
     gen.register_app::<Chopin>()?;
 
-    //let output_root = PathBuf::from("./generated");
+    let output_root = PathBuf::from("./generated");
 
-    // gen.swift("SharedTypes", output_root.join("swift"))?;
+    gen.swift("SharedTypes", output_root.join("swift"))?;
 
     // gen.java("com.example.counter.shared_types", output_root.join("java"))?;
 
