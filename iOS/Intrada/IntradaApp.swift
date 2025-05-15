50c3115f018cb5485a08e9bd5f53e7518de1b9e2
Jon Yardley <jonyardley@me.com>
2025-05-15 18:52:00 +0100

    Initial iOS setup

---
+++ iOS/Intrada/IntradaApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct IntradaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            //ContentView(core: Core())
+            ContentView()
+        }
+    }
+}
