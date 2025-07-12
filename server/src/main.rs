059e8eb35434c645dd622abeebb25d9488e16863
Jon Yardley <jon.yardley@red-badger.com>
2025-07-12 15:21:28 +0100

    Try this

--- server/src/main.rs
+++ server/src/main.rs
@@ -26,6 +26,6 @@ async fn main() {
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
 
-    println!("Server running on http://0.0.0.0:{}", port);
+    println!("Server running on http://0.0.0.0:{port}");
     axum::serve(listener, app).await.unwrap();
 }
